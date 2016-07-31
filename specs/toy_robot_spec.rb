99802b6acc4eb7c09b373d7f938952e1eefa819c
oschebell <oschebell@gmail.com>
2016-07-31 17:36:51 +1000

    Implement move

--- specs/toy_robot_spec.rb
+++ specs/toy_robot_spec.rb
@@ -10,11 +10,40 @@ RSpec.describe ToyRobot, "#place" do
   end
 end
 
-
 RSpec.describe ToyRobot, "#report" do
   it "will print coordinates and direction" do
     toy_robot = ToyRobot.new
     toy_robot.place(0, 0, "NORTH")
     expect(toy_robot.report).to eq("0, 0, NORTH")
   end
+end
+
+RSpec.describe ToyRobot, "#move" do
+    it "will move the robot 1 unit north" do
+      toy_robot = ToyRobot.new
+      toy_robot.place(0, 0, "NORTH")
+      toy_robot.move
+      expect(toy_robot.report).to eq("0, 1, NORTH")
+    end
+
+    it "will move the robot 1 unit east" do
+      toy_robot = ToyRobot.new
+      toy_robot.place(0, 0, "EAST")
+      toy_robot.move
+      expect(toy_robot.report).to eq("1, 0, EAST")
+    end
+
+    it "will move the robot 1 unit south" do
+      toy_robot = ToyRobot.new
+      toy_robot.place(0, 1, "SOUTH")
+      toy_robot.move
+      expect(toy_robot.report).to eq("0, 0, SOUTH")
+    end
+
+    it "will move the robot 1 unit west" do
+      toy_robot = ToyRobot.new
+      toy_robot.place(1, 0, "WEST")
+      toy_robot.move
+      expect(toy_robot.report).to eq("0, 0, WEST")
+    end
 end
--- toy_robot.rb
+++ toy_robot.rb
@@ -8,4 +8,16 @@ class ToyRobot
   def report
     return "#{@x}, #{@y}, #{@direction}"
   end
+
+  def move
+    if @direction == "EAST"
+      @x = @x + 1
+    elsif @direction == "NORTH"
+      @y = @y + 1
+    elsif @direction == "SOUTH"
+      @y = @y - 1
+    elsif @direction == "WEST"
+      @x = @x - 1
+    end
+  end
 end
